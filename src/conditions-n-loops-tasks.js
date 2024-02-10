908c7d4aac92767d47dd2242b5b28ebab005f3fa
Svetlana <claire-an@mail.ru>
2024-02-10 17:35:06 +0500

    feat: add solution shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -388,8 +388,22 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let countIteration = iterations;
+  let resultStr = str;
+  let k = 0;
+  while (k < countIteration) {
+    let result = '';
+    for (let i = 0; i < resultStr.length; i += 2) result += resultStr[i];
+    for (let i = 1; i < resultStr.length; i += 2) result += resultStr[i];
+    resultStr = result;
+    k += 1;
+    if (resultStr === str) {
+      countIteration %= k;
+      k = 0;
+    }
+  }
+  return resultStr;
 }
 
 /**
